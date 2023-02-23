bd1217968c32973820cbdd7fe670340edf13c468
s_death_s <urvanova03@mail.ru>
2023-02-23 17:16:31 +0300

    feat: add init resources in MainActivity.java

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textAuthorization = findViewById(R.id.textAuthorization);
+        textAuthorization.setText(R.string.authorization);
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.logo);
     }
 }
