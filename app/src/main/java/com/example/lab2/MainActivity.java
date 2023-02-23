030ee00138a155204bf9d010c949e49f2c20a7ca
s_death_s <urvanova03@mail.ru>
2023-02-23 17:42:10 +0300

    feat: using View Binding

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,24 +3,29 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.lab2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        TextView textAuthorization = findViewById(R.id.textAuthorization);
-        textAuthorization.setText(R.string.authorization);
-        ImageView imageView = findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.logo);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+//        TextView textAuthorization = findViewById(R.id.textAuthorization);
+        binding.textAuthorization.setText(R.string.authorization);
+//        ImageView imageView = findViewById(R.id.imageView);
+        binding.imageView.setImageResource(R.drawable.logo);
+//        Button button = findViewById(R.id.button);
+        binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("onClick", "button clicked");
