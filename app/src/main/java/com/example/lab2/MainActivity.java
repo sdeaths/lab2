047130e103a90e43def0bb6bd7be9b5c4297c5d0
s_death_s <urvanova03@mail.ru>
2023-02-23 17:32:28 +0300

    feat: add programmatic log on click

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,9 +19,17 @@ public class MainActivity extends AppCompatActivity {
         textAuthorization.setText(R.string.authorization);
         ImageView imageView = findViewById(R.id.imageView);
         imageView.setImageResource(R.drawable.logo);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("onClick", "button clicked");
+            }
+        });
     }
 
-    public void logInClick(View view) {
-        Log.d("onClick", "button clicked");
-    }
+//    public void logInClick(View view) {
+//        Log.d("onClick", "button clicked");
+//    }
+
 }
