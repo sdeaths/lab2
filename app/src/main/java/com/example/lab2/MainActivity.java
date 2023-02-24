ae266801496bc7c4c0359d3ee6cdb4c06893bef9
s_death_s <urvanova03@mail.ru>
2023-02-24 16:44:15 +0300

    feat: add second activity

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.lab2;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.util.Log;
@@ -14,7 +19,18 @@ import com.example.lab2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-
+    ActivityResultLauncher<Intent> startForResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result != null && result.getResultCode() == RESULT_OK) {
+                        if (result.getData() != null && result.getData().getStringExtra(UserActivity.KEY) != null) {
+                            binding.textPhone.setText(result.getData().getStringExtra(UserActivity.KEY));
+                            binding.password.setText("");
+                        }
+                    }
+                }
+            });
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +45,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d("onClick", "button clicked");
+                startForResult.launch(UserActivity.newUsrActivity(MainActivity.this, binding.textPhone.getText().toString()));
             }
         });
+
     }
 
 //    public void logInClick(View view) {
