d5df715276dcf852b4e1def0ba66e417c4d13353
Elizaveta Urvanova <urvanova03@mail.ru>
2023-03-02 17:50:23 +0300

    feat: add log and toast

--- app/src/main/java/com/example/lab2/WelcomeScreenFragment.java
+++ app/src/main/java/com/example/lab2/WelcomeScreenFragment.java
@@ -1,9 +1,12 @@
 package com.example.lab2;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,15 +18,36 @@ import com.example.lab2.databinding.FragmentWelcomeScreenBinding;
 public class WelcomeScreenFragment extends Fragment {
     private FragmentWelcomeScreenBinding binding;
 
+    private static final String TAG = "WelcomeScreenFragment";
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        Toast.makeText(getContext(), "onAttach", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onAttach");
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Toast.makeText(getContext(), "onCreate", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+    }
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Toast.makeText(getContext(), "onCreateView", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onCreateView");
         binding = FragmentWelcomeScreenBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Toast.makeText(getContext(), "onViewCreated", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onViewCreated");
         super.onViewCreated(view, savedInstanceState);
         binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,5 +58,68 @@ public class WelcomeScreenFragment extends Fragment {
                         .commit();
             }
         });
+    }
+
+    @Override
+    public void onViewStateRestored(@Nullable Bundle savedInstanceState) {
+        Toast.makeText(getContext(), "onViewStateRestored", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onViewStateRestored");
+        super.onViewStateRestored(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Toast.makeText(getContext(), "onStart", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Toast.makeText(getContext(), "onResume", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Toast.makeText(getContext(), "onPause", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Toast.makeText(getContext(), "onStop", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        Toast.makeText(getContext(), "onSaveInstanceState", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onSaveInstanceState");
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        Toast.makeText(getContext(), "onDestroyView", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onDestroyView");
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(getContext(), "onDestroy", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onDestroy");
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        Toast.makeText(getContext(), "onDetach", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onDetach");
+        super.onDetach();
     }
 }
